671502c0dc927d29cd71e39c2afe19f0d5062566
Jake Danton <jacob.danton@gmail.com>
2026-04-09 10:16:41 -0700

    fixing fish homebrew config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 if test -x /opt/homebrew/bin/brew
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+    /opt/homebrew/bin/brew shellenv | source
 else if test -x /usr/local/bin/brew
-  eval "$(/usr/local/bin/brew shellenv)"
+    /usr/local/bin/brew shellenv | source
 else if test -x /home/linuxbrew/.linuxbrew/bin/brew
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
 end
 
 starship init fish | source
